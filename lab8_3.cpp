087e6649850147e4a5182ab701e5db15702c67f8
TANGCPE64 <natsuphat_th@cmu.ac.th>
2021-12-03 16:57:19 +0700

    Completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,9 +1,31 @@
 #include<iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
+char before(char x)
+{
+	if( 'A' < x )
+	{
+		return x-1;
+	}
+	if( 'Z' == x )
+	{
+		return x+1;
+	}
+	if( 'A' == x )
+	{
+		return 'Z';
+	}
+	if( 'A' > x )
+	{
+		return 0;
+	}
+	if ( 'Z' < x)
+	{
+		return 0;
+	}
+	
 }
+
 
 int main(){
 	//Test Case
